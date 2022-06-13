d0a9f2c313762a1d1dfd07cfc84123e8a8d25173
DavidMacBook <davidmoralesluis@gmail.com>
2022-06-13 22:51:46 +0200

    Fichero commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,7 +7,36 @@ public class Main {
     PrintWriter printW;
     FileWriter fileW;
     public static void main(String[] args) throws IOException {
+        Main main = new Main();
+        String token = "/Users/davidmoralesluis/Documents/VisualStudioCode/GitHub/token";
+        File file = new File(token);
+        if (file.exists()) {
+            main.creacionRepositorio(token);
+        } else {
 
+            main.creacionRepositorio(token);
+        }
+    }
+
+    public  void creacionRepositorio(String fichero){
+        GitHub github = null;
+        try {
+            new GitHubBuilder();
+            github = GitHubBuilder
+                    .fromPropertyFile(fichero)
+                    .build();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+        try {
+            if (github != null) {
+                GHRepository repo1 = github.createRepository(
+                                "GithubAPI")
+                        .create();
+            }
+        } catch (IOException e) {
+            System.out.println("Ya existe");
+        }
     }
 
 
